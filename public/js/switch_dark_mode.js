b4c7cbd5982e143167ab36354cf17585520a2c76
JoviOakz <joao.mateussdc@gmail.com>
2024-04-02 15:55:51 -0300

    Aplicando dark mode para paginas seguintes

--- public/js/switch_dark_mode.js
+++ public/js/switch_dark_mode.js
@@ -1,9 +1,24 @@
-const themeSwitch = document.getElementById('colorSwitch');
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
 
-themeSwitch.addEventListener('change', function () {
-    if (this.checked) {
-        document.documentElement.classList.add('dark-mode');
+    // Armazena o estado do modo escuro no localStorage
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+}
+
+function applyDarkMode() {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    const body = document.body;
+    const darkModeSwitch = document.getElementById('darkModeSwitch');
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+        darkModeSwitch.checked = true;
     } else {
-        document.documentElement.classList.remove('dark-mode');
+        body.classList.remove('dark-mode');
+        darkModeSwitch.checked = false;
     }
-});
+}
+
+// Aplica o modo escuro ao carregar a página
+window.onload = applyDarkMode;
