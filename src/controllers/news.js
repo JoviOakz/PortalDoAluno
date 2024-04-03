11e715ce25022e64d8fd54f5b4703620a9ecc975
JoviOakz <joao.mateussdc@gmail.com>
2024-04-03 16:49:34 -0300

    Tentativa de transformar nome e curso em constante para todas as páginas conterem os dados da pessoa

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -4,24 +4,28 @@ const turma = require('../model/turma');
 const materia = require('../model/materia');
 const pessoa = require('../model/pessoa');
 
-const url = require('url');
+// const url = require('url');
 
-const urlString = process.argv[3];
+// const urlString = process.argv[3];
 
-// Supondo que você esteja executando isso em um servidor HTTP do Node.js
-// req é o objeto de solicitação HTTP
-const { query } = url.parse(urlString, true);
+// // Supondo que você esteja executando isso em um servidor HTTP do Node.js
+// // req é o objeto de solicitação HTTP
+// const { query } = url.parse(urlString, true);
 
-// Obtendo valores específicos dos parâmetros
-const idPessoa = query.idPessoa;
-const selecionadoMM = query.selecionado;
-const funcionario = query.funcionario;
+// // Obtendo valores específicos dos parâmetros
+// const idPessoa = query.idPessoa;
+// const selecionadoMM = query.selecionado;
+// const funcionario = query.funcionario;
 
 
 
 module.exports = {
     
     async dados(req, res) {
+        const idPessoa = req.query.idPessoa;
+        const selecionadoMM = req.query.selecionado;
+        const funcionario = req.query.funcionario;
+
         const encontrarPessoa = await pessoa.findOne({
             raw: true,
             attributes: ['IDPessoa', 'Nome'],
