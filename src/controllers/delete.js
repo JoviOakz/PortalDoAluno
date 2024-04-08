299daebf9bd91300d610a329dbf662f86bd87e5b
JoviOakz <joao.mateussdc@gmail.com>
2024-04-08 13:57:09 -0300

    Delete aluno criado (em desenvolvimento)

---
+++ src/controllers/delete.js
@@ -0,0 +1,9 @@
+const matricula = require('../model/matricula');
+const curso = require('../model/curso');
+const turma = require('../model/turma');
+const materia = require('../model/materia');
+const pessoa = require('../model/pessoa');
+
+module.exports = {
+    
+}
